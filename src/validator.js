de90669368bd96042bc3220a77c334abbe7ac531
unknown <lis2310.llla@gmail.com>
2022-05-17 08:49:33 -0500

    realizando la logica de mi programa

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,18 @@
 const validator = {
   // ...
+
+  isValid:function(creditCardNumber){
+    var array=[creditCardNumber];
+    var newarray=[[0][creditCardNumber.length-1]];
+    for(var i=creditCardNumber.length-2;i>0;i--){
+      newarray.push(array[0][i]);
+
+    }
+
+
+  },
+  maskify:function(){}
+
 };
 
 export default validator;
