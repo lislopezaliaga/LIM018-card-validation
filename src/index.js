6333ad34fd5af5c14de0afafe0b57dcb892806d3
unknown <lis2310.llla@gmail.com>
2022-05-23 09:49:43 -0500

    changed

--- src/index.js
+++ src/index.js
@@ -2,4 +2,78 @@ import validator from './validator.js';
 
 console.log(validator);
 
+const pays=document.querySelector("#shop");
+pays.addEventListener("click",function(evento){
+        document.getElementById("cabeza").style.display = "none";
+        document.getElementById("divfirst").style.display = "none";
+        document.getElementById("div2").style.display = "block";
+    
+});
+
+const get=document.querySelector("#get");
+get.addEventListener("click",function(evento){
+        document.getElementById("cabeza").style.display = "none";
+        document.getElementById("divfirst").style.display = "none";
+        document.getElementById("div2").style.display = "block";
+    
+});
+
+const valid=document.querySelector("#validar");
+valid.addEventListener("click",function(evento){
+    
+
+    document.getElementById("div2").style.display = "none";
+
+    let creditCardNumber=document.getElementById("number").value;
+    let isValidFirst = validator.isValid(creditCardNumber);
+    let maskify=validator.maskify(creditCardNumber);
+
+    let name=document.getElementById("name").value; 
+    let lastName=document.getElementById("lastName").value;
+    let dni=document.getElementById("dni").value;
+    
+    
+    if(isValidFirst==true){
+        document.getElementById("last").style.display = "block";
+        document.getElementById("numb").innerHTML="Número de tarjeta:"+maskify;
+        document.getElementById("printName").innerHTML="Nombre:"+name;
+        document.getElementById("printLastName").innerHTML="Apellido:"+lastName;
+        document.getElementById("printDni").innerHTML="DNI:"+dni;
+
+
+
+    }else{
+        document.getElementById("sorry").style.display = "block";
+    }
+
+    
+        
+});
+
+
+
+
+
+const buttonAdd=document.querySelectorAll('.button-add');
+buttonAdd.forEach((addToCardButton) =>{
+    addToCardButton.addEventListener('click',function(evento){
+        console.log(+1);
+    })
+});
+
+const buttonReturns=document.querySelector("#buttonReturn");
+buttonReturns.addEventListener("click",function(evento){
+        document.getElementById("cabeza").style.display = "none";
+        document.getElementById("divfirst").style.display = "none";
+        document.getElementById("div2").style.display = "block";
+    
+});
+const buttonBacks=document.querySelector("#buttonBackShop");
+buttonBacks.addEventListener("click",function(evento){
+        document.getElementById("cabeza").style.display = "none";
+        document.getElementById("divfirst").style.display = "none";
+        document.getElementById("div2").style.display = "block";
+    
+});
+    
 
--- src/validator.js
+++ src/validator.js
@@ -2,16 +2,49 @@ const validator = {
   // ...
 
   isValid:function(creditCardNumber){
-    var array=[creditCardNumber];
-    var newarray=[[0][creditCardNumber.length-1]];
-    for(var i=creditCardNumber.length-2;i>0;i--){
-      newarray.push(array[0][i]);
+    const array=creditCardNumber.split('').reverse();
+    
+    const newarray=[];
 
+    for(var i=0;i<array.length;i++){
+      if(i%2!=0){
+        var multiplication=array[i]*2;
+        if(multiplication>9){
+          var convertString=multiplication.toString();
+          var result=parseInt(convertString.charAt(0))+parseInt(convertString.charAt(1));
+          newarray.push(result);
+        }else{
+          newarray.push(multiplication);
+        }
+      }else{
+        newarray.push(array[i]);
+      }
+      
     }
 
+    var additions=0;
+    for( i=0;i<newarray.length;i++){
+      additions=additions+parseInt(newarray[i]);
+    }
+    if(additions%10==0){
+      return true;
+    }else{
+      return false;
+    }
 
   },
-  maskify:function(){}
+  maskify:function(creditCardNumber){
+    let four=creditCardNumber.slice(0,creditCardNumber.length-4)
+    let secondArray=[];
+    for(var j=0;j<four.length;j++){
+      secondArray.push("#");
+
+    }
+    let textComplete=secondArray.join("")+creditCardNumber.slice(-4);
+    return textComplete;
+    
+
+  }
 
 };
 
